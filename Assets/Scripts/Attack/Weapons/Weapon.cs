a73e67025ab6d4173468873bf234db7d15f8d8f1
Vladislav <boxofmatic@gmail.com>
2022-09-19 12:17:26 +0300

    weapons +

---
+++ Assets/Scripts/Attack/Weapons/Weapon.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NaughtyAttributes;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Weapon : MonoBehaviour {
+
+    [Header("Main")]
+    [SerializeField] private WeaponData _weaponData;
+
+    [Header("Transforms")] 
+    [SerializeField] private Transform _shootPointT;
+    
+    [Header("Options")]
+    [ShowIf("_isFirearmsMode"), SerializeField] private GameObject _projectile;
+    [SerializeField] private bool _attackClosestEnemy = true;
+    [SerializeField] private bool _autoSearchEnemies = true; // TODO: думаю можно это сделать абстрактным
+    [ShowIf("_autoSearchEnemies"), SerializeField] private LayerMask _enemiesLayer;
+
+    private bool _attackEnabled = true;
+    
+    private Timer _autoAttackTimer = new Timer(1);
+
+    public Action OnAttack;
+    
+    private bool _isMeleeMode;
+    private bool _isFirearmsMode;
+
+    #region getters/setters
+
+    public bool AttackEnabled {
+        get => _attackEnabled;
+        set => _attackEnabled = value;
+    }
+
+    public WeaponData WeaponData {
+        get => _weaponData;
+        set {
+            _weaponData = value;
+            
+            _autoAttackTimer.ChangeInitTime(_weaponData.AttackDelay);
+        }
+    }
+
+    #endregion
+    
+    private void Awake() {
+        if (_weaponData != null) {
+            WeaponData = _weaponData;
+        }
+    }
+
+    private void Update() {
+        if (_autoSearchEnemies) {
+            
+            _autoAttackTimer.Update(Time.deltaTime);
+
+            if (_autoAttackTimer.IsFinish()) {
+                
+                SearchEnemies(_attackClosestEnemy);
+                
+                _autoAttackTimer.Reload();
+            }
+        }
+    }
+    
+    // метод некорректно имеет метку Perfomance Critical Context в райдере потому что он вызывается из Update внутри таймера
+    private List<Entity> _foundedEnemies = new List<Entity>();
+    private void SearchEnemies(bool attackClosest = true) {
+        
+        _foundedEnemies.Clear(); // это гораздо оптимизированнее, чем создавать новый список внутри метода
+        
+        var hits = Physics.SphereCastAll(
+            transform.position, _weaponData.AttackRange, transform.forward,
+            _weaponData.AttackRange, _enemiesLayer);
+
+        foreach (var hit in hits) {
+            // не переживайте, GetComponent с недавних пор кэшируется и он довольно быстрый
+            var entity = hit.collider.attachedRigidbody.gameObject.GetComponent<Entity>();
+
+            if (entity == null) {
+                Debug.Log("Entity strangely missed!");
+                continue;
+            }
+            
+            _foundedEnemies.Add(entity);
+        }
+
+        if (_foundedEnemies.Count > 0) {
+            if (attackClosest) {
+                // а вот это не очень оптимизированно, майскрософт обещает разобраться с аллоками в будущих версиях LINQ
+                // но в целом они не такие страшные что бы отказаться от использования
+                _foundedEnemies = _foundedEnemies.OrderBy(
+                    x => Vector3.Distance(this.transform.position, x.transform.position)
+                ).ToList();
+
+                Attack(_foundedEnemies[0]);
+
+            } else {
+                
+                // атакует случайного противника
+                Attack(_foundedEnemies[Random.Range(0, _foundedEnemies.Count)]);
+            }
+        }
+    }
+    
+    public void Attack(Entity entity) {
+
+        OnAttack.Invoke();
+
+        switch (_weaponData.AttackMode) {
+            case WeaponData.ATTACK_MODE.FIREARMS:
+                //var projectile = PrefabsCreator.Instance.
+                break;
+        }
+    }
+    
+    private void OnValidate() {
+        if (_weaponData == null) {
+            return;
+        }
+        
+        _isMeleeMode = _weaponData.AttackMode == WeaponData.ATTACK_MODE.MELEE;
+        _isFirearmsMode = _weaponData.AttackMode == WeaponData.ATTACK_MODE.FIREARMS;
+    }
+
+    private void OnDrawGizmos() {
+        if (_weaponData == null) {
+            return;
+        }
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, _weaponData.AttackRange);
+    }
+}
